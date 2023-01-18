52a84d723646f2a406156cee7d1674898230a98d
OutlookBlo <104131740+OutlookBlo@users.noreply.github.com>
2023-01-18 20:15:22 +1100

    Update main.js

--- main.js
+++ main.js
@@ -13,9 +13,23 @@ recognition.onresult = function(event)
     console.log(event);
 
     var Content = event.results[0][0].transcript;
-    console.log(Content);
+document.getElementById("textbox").innerHTML = Content;
 
-    document.getElementById("textbox").innerHTML = Content;
+console.log(Content);
+    speak();
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+
+    speak_data = document.getElementById("textbox").value;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
 }
 
 Webcam.set
